2b06597314906278adbc21fb5ab50489da6e3e3a
Jeff Rescignano <jeffresc@icloud.com>
2018-07-14 08:10:18 -0400

    Remove debug

--- KikContactPhotoProvider.m
+++ KikContactPhotoProvider.m
@@ -1,5 +1,4 @@
 #import "KikContactPhotoProvider.h"
-#import <Foundation/Foundation.h>
 
 @implementation KikContactPhotoProvider
 
@@ -9,7 +8,6 @@
 	NSArray *parsedProfile = [profileID componentsSeparatedByString:@"_"];
 	NSString *username = [parsedProfile firstObject];
     NSString *profileURLStr = [NSString stringWithFormat: @"http://cdn.kik.com/user/pic/%@", username];
-	NSLog(@"FIRED FIRED FIRED FIRED FIRED FIRED FIRED FIRED FIRED FIRED FIRED FIRED FIRED: %@.", profileURLStr);
     NSURL *profileURL = [NSURL URLWithString:profileURLStr];
     if (!profileURL) return nil;
     return [NSClassFromString(@"DDNotificationContactPhotoPromiseOffer") offerDownloadingPromiseWithPhotoIdentifier:profileURLStr fromURL:profileURL];
